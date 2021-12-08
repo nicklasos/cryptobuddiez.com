2bf3a3645649282cf5f29ea8d47264a5d7870968
nicklasos <github7415963>
2021-12-08 16:35:11 +0200

    *

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,25 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                background: '#d5d5d5',
+                background: '#fff',
+                'black-background': '#1F1F1F',
+                'gray-text': '#888888',
+                'lightgray-text': '#565656',
+                'lightgray-text-header': '#C5C5C5'
             },
             fontFamily: {
                 'roboto': ['Roboto', 'ui-sans-serif', 'serif'],
+                'roboto-mono': ['Roboto Mono', 'ui-sans-serif', 'serif'],
+                'Poppins': ['Poppins', 'ui-sans-serif', 'serif'],
             },
             spacing: {
-                '150': '36rem',
-            }
+                15: '3.75rem',
+                22: '5.5rem',
+                150: '36rem',
+            },
+            fontSize: {
+                '4.5xl': ['2.75rem', { lineHeight: '2.5rem' }],
+            },
         },
     },
     variants: {
