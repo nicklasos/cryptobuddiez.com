0cef7473f3e85cab7ce46884f6e522b29930efde
nicklasos <github7415963>
2022-01-17 10:45:24 +0200

    Fixes and improvements

--- public/js/app.js
+++ public/js/app.js
@@ -7,10 +7,23 @@
   \**************************/
 /***/ (() => {
 
-var btn = document.querySelector('.mobile-menu-button');
-btn.addEventListener('click', function () {
+var menuBtn = document.querySelector('.mobile-menu-button');
+menuBtn.addEventListener('click', function () {
   document.body.classList.toggle('mobile-menu-open');
 });
+var mobileMenuBtns = document.querySelector('.mobile-menu-btns');
+mobileMenuBtns.addEventListener('click', function () {
+  document.body.classList.remove('mobile-menu-open');
+});
+var mobileRarityMenuBtn = document.querySelector('.mobile-rarity-menu-btn');
+mobileRarityMenuBtn.addEventListener('click', function () {
+  document.body.classList.toggle('mobile-rarity-menu-open');
+});
+var teammates = document.querySelector('.teammate');
+teammates.addEventListener('mousemove', function (ev) {
+  document.querySelector('.balloon').style.transform = 'translateY(' + (ev.clientY - 200) + 'px)';
+  document.querySelector('.balloon').style.transform += 'translateX(' + (ev.clientX - 100) + 'px)';
+}, false);
 
 /***/ }),
 
